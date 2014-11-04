99ce573dd6cd9b90ccee22c98fd55cbf7bdd432c
al222jf <al222jf@student.lnu.se>
2014-11-04 19:27:05 +0100

    jobbat lite med if satser till guess

--- 1dv403-laborationer-master/1-vandalen/guess/script.js
+++ 1dv403-laborationer-master/1-vandalen/guess/script.js
@@ -3,6 +3,7 @@
 window.onload = function(){
 	
 	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var counter = 0;
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
@@ -10,8 +11,22 @@ window.onload = function(){
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
 		// Plats för förändring.
+		counter++;
+		if (number === secret) {
+			return [true, "Grattis du vann! Det hemliga talet var" + secret + "och du behövde" + counter + "gissningar för att hitta det."];
+		}
 
+		if (number < 0 || number > 100) {
+			return [false, "Talet är utanför intervallet 0 - 100"];
+		};
 
+		if (number < secret) {
+			return [false, "Det hemliga talet är högre!"];
+		};
+
+		if (number > secret) {
+			return [false, "Det hemliga talet är lägre!"];
+		};
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
 		// [false, "Det hemliga talet är högre!"]
