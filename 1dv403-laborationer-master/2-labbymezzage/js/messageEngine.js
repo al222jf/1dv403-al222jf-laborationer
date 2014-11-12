5383edba4273fce2af06d9f2072abc96a21cf6c2
al222jf <al222jf@student.lnu.se>
2014-11-12 19:49:45 +0100

    end of day commit

--- 1dv403-laborationer-master/2-labbymezzage/js/messageEngine.js
+++ 1dv403-laborationer-master/2-labbymezzage/js/messageEngine.js
@@ -1,14 +1,17 @@
 "use strict";
 
 var myApp = {
-	init:function(){
+
+	messages: [],
+	console.log(messages);
+	init:function(e){
+		var mess = new Message("Testmeddelande", new Date());
+
+		alert(mess);
+		mess.setText("En annan text");
+		alert(mess);
+		messages.push(mess);
 	}
-};
-
-
-
-
-
-
+}
 
 window.onload = myApp.init;
