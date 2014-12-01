5b264aeb3dcb2815cc643fd8409379b3e4028521
al222jf <al222jf@student.lnu.se>
2014-12-01 15:27:36 +0100

    loopar ut bilderna

--- 1dv403-laborationer-master/3-gameon/memory/script/gameon.js
+++ 1dv403-laborationer-master/3-gameon/memory/script/gameon.js
@@ -7,8 +7,39 @@ var GameOn = {
 		var memoryArr;
 
 		memoryArr = RandomGenerator.getPictureArray(rows, cols);
-		console.log(memoryArr);
-	}
+		console.log(memoryArr.length);
+		GameOn.setClass(memoryArr);
+
+
+	},
+
+	setClass: function(memoryArr){
+		var picDiv;
+		var getDivId;
+		var imgTag;
+		
+		for(var i = 0; i < memoryArr.length; i += 1){
+			
+			getDivId = document.getElementById("memoryGame");
+			picDiv = document.createElement("div");
+			imgTag = document.createElement("img");
+
+			picDiv.setAttribute("class", "tile");
+			picDiv.setAttribute("rel", memoryArr[i]);
+			imgTag.setAttribute("src", "pics/0.png")
+
+			getDivId.appendChild(picDiv);
+			picDiv.appendChild(imgTag);
+
+
+		}
+
+	},
+
+	counter: function(){
+		//Ska räkna antal gjorda drag
+	},
+	
 
 
 };
